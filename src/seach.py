20646bd36c369c7673366904fe8cf5b728e78721
klxcoder <klxcoder@gmail.com>
2025-03-25 20:24:09 +0700

    get_flags

--- src/seach.py
+++ src/seach.py
@@ -20,8 +20,32 @@ def find_start(arr):
                 return (r, c)
     return None
 
+def get_flags(arr):
+    n_row = len(arr)
+    n_col = len(arr[0])
+    flags = []
+    for r in range(n_row):
+        row = []
+        for c in range(n_col):
+            row.append(False)
+        flags.append(row)
+    return flags
+
+def find_shortest_bfs_path(arr, start):
+    row = len(arr)
+    col = len(arr[0])
+    frontier = [start]
+    cost = 0
+    while True:
+        first = frontier.pop(0)
+
+
 if __name__ == "__main__":
     arr = read_arr("src/src0/maze1.txt")
     print_arr(arr)
     start = find_start(arr)
     print(start)
+    flags = get_flags(arr)
+    print_arr(flags)
+    shortest_bfs_path = find_shortest_bfs_path(arr)
+    print(shortest_bfs_path)
