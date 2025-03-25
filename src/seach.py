73482747c1e50df8043d882a247c0cd7bd9134a0
klxcoder <klxcoder@gmail.com>
2025-03-25 20:01:10 +0700

    read_arr

--- src/seach.py
+++ src/seach.py
@@ -1,6 +1,10 @@
 def read_arr(path):
-    return path
+    arr = []
+    with open(path, 'r') as file:
+        for line in file:
+            arr.append(line.strip()) # `.strip()` removes leading/trailing spaces and newlines
+    return arr
 
 if __name__ == "__main__":
-    arr = read_arr("src0/maze1.txt")
+    arr = read_arr("src/src0/maze1.txt")
     print(arr)
