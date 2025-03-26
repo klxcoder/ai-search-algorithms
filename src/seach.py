72fd7694cb01cd9167a232e933160870ca28e64a
klxcoder <klxcoder@gmail.com>
2025-03-26 07:57:05 +0700

    self.back

--- src/seach.py
+++ src/seach.py
@@ -101,23 +101,23 @@ class BFS:
         self.arr = arr
         self.start = start
         self.frontier = deque([self.start])
+        self.back = {}
 
     def run(self):
         flags = get_flags(self.arr)
-        back = {}
         while len(self.frontier) != 0:
             # popleft -> bfs
             # pop -> dfs
             first = self.frontier.popleft()
             flags[first[0]][first[1]] = '1'
             if self.arr[first[0]][first[1]] == 'B':
-                path = backtrack(first, self.start, back, self.arr)
+                path = backtrack(first, self.start, self.back, self.arr)
                 show_arr(self.arr, self.start, first, path)
                 break
             adjacent_cells = get_adjacent_cells(first, flags, self.arr)
             self.frontier += adjacent_cells
             for cell in adjacent_cells:
-                back[cell] = first
+                self.back[cell] = first
 
 
 if __name__ == "__main__":
